f9d7e3af4366057993a7f703d2b2f62a619b216c
alonzae <rushclass12>
2014-01-24 14:45:55 -0600

    SOTD
    Socks of the day!!!!!!!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SOTD::Application.config.secret_key_base = '2ee2211a69cb392213efd58d817eabcc063186e63bd88d862e07101cb7137790a7007e40ff7288484340432e9bfba41ed1dae41a06df8a0cd9bbc9c1a014555b'
